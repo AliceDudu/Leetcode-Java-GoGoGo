5ec02b093945b9c8de739e4e59a3bdb3aee6582d
Alice Yang <ligangmm@gmail.com>
2016-12-16 19:21:06 -0600

    Update and rename 369-Plus One Linked List.java to Week01/369-Plus One Linked List.java

--- Week01/369-Plus One Linked List.java
+++ Week01/369-Plus One Linked List.java
@@ -1,3 +1,18 @@
+/*
+题目链接: https://leetcode.com/problems/plus-one-linked-list/
+
+Given a non-negative number represented as a singly linked list of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+Input:
+1->2->3
+
+Output:
+1->2->4
+*/
+
 public class PlusOneListSolution {
 	public ListNode plusOne(ListNode head){
 		//corner case
