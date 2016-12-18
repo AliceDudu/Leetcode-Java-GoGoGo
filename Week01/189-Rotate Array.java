06ad960f857af41898c50d7231ca0813cc49b4b2
Alice Yang <ligangmm@gmail.com>
2016-12-18 11:00:05 -0600

    Add files via upload

---
+++ Week01/189-Rotate Array.java
@@ -0,0 +1,48 @@
+public class Solution {
+	public void rotate(int[] nums, int k){
+		if ( nums == null || nums.length ==1 )
+			return nums;
+		
+		int n = nums.length;
+		k = k % n;
+		
+		reverse(nums, 0, n-k-1);
+		reverse(nums, n-k, n-1);
+		reverse(nums, 0, n-1);
+	}
+	
+	public void reverse(int[] nums, int left, int right){		
+		int tmp;
+		while ( left < right ){
+			tmp = nums[left];
+			nums[left] = nums[right];
+			nums[right] = tmp;
+			left++;
+			right--;
+		}
+	}
+	
+}
+
+
+// public class Solution {
+// 	public void rotate(int[] nums, int k){
+// 		if ( nums == null || nums.length == 1 )
+// 			return;
+// 		
+// 		int n = nums.length;
+// 		
+// 		int[] numsCopy = new int[n];
+// 		
+// 		for ( int i = 0; i < n; i ++ ){
+// 			numsCopy[i] = nums[i];
+// 		}
+// 		
+// 		for ( int i = 0; i < n; i ++ ){
+// 			nums[ (i + k) % n ] = numsCopy[i];
+// 		}
+// 			
+// 	}
+// }
+
+
