7c938e5d6ece2a04c2bc12a603f80cc9ed315d6a
Alice Yang <ligangmm@gmail.com>
2016-12-28 09:42:25 -0600

    Add files via upload

---
+++ 71-Simplify Path.java
@@ -0,0 +1,22 @@
+public class Solution{
+	public String simplifyPath(String path){
+		
+		Deque<String> stack = new ArrayDeque<>();
+		Set<String> skip = new HashSet<>( Arrays.asList("..",".","") );
+		
+		for( String dir : path.split("/") ){
+			if( dir.equals("..") && !stack.isEmpty() )
+				stack.pop();
+			else if( !skip.contains(dir) )
+				stack.push(dir);
+		}
+		
+		String result = "";
+		
+		for( String dir : stack )
+			result = "/" + dir + result;
+		
+		return result.isEmpty() ? "/" : result;
+		
+	}
+}
