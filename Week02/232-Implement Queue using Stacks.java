7f3469b20238d2b66fe724cd086ded8941dbaf87
Alice Yang <ligangmm@gmail.com>
2016-12-29 15:37:13 -0600

    Add files via upload

---
+++ Week02/232-Implement Queue using Stacks.java
@@ -0,0 +1,33 @@
+class MyQueue{
+
+	Deque<Integer> stack = new ArrayDeque<>();
+	
+	//queue = [1,2,3]
+	//stack = [3,2,1]
+
+	public void push(int x){
+		Deque<Integer> temp = new ArrayDeque<>();
+		
+		while ( !stack.isEmpty() )
+			temp.push( stack.pop() );
+			
+		stack.push(x);
+		
+		while ( !temp.isEmpty() )
+			stack.push( temp.pop() );
+	
+	}
+	
+	public void pop(){
+		stack.pop();
+	}
+	
+	public int peek(){
+		return stack.peek();
+	}
+	
+	public boolean empty(){
+		return stack.isEmpty();
+	}
+	
+}
