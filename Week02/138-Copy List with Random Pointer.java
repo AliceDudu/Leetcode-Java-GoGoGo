1d93ce6620d3aeb62dc6953194522f374e092cd8
Alice Yang <ligangmm@gmail.com>
2017-01-02 10:57:37 -0600

    Add files via upload

--- Week02/138-Copy List with Random Pointer.java
+++ Week02/138-Copy List with Random Pointer.java
@@ -7,26 +7,77 @@
  * };
  */
 
+
 public class Solution{
 	public RandomListNode copyRandomList(RandomListNode head){
+	
 		if ( head == null )
-			return null;
+			return head;
 		
 		Map<RandomListNode, RandomListNode> map = new HashMap<>();
 		
-		RandomListNode node = head;
-		while ( node != null ){
-			map.put(node, new RandomListNode(node.label));
-			node = node.next;
-		}
+		RandomListNode cur = head;
 		
-		node = head;
-		while ( node != null ){
-			map.get(node).next = map.get(node.next);
-			map.get(node).random = map.get(node.random);
-			node = node.next;
+		
+		while ( cur != null ){
+		
+			// copy cur
+			if ( !map.containsKey(cur) ){
+				RandomListNode curCopy = new RandomListNode(cur.label);
+				map.put(cur, curCopy);
+			}
+		
+			// copy cur.next
+			if ( cur.next != null && !map.containsKey(cur.next) ){
+				RandomListNode nextCopy = new RandomListNode(cur.next.label);
+				map.put(cur.next, nextCopy);
+			}
+			
+			// copy cur.random
+			if ( cur.random != null && !map.containsKey(cur.random) ){
+				RandomListNode randomCopy = new RandomListNode(cur.random.label);
+				map.put(cur.random, randomCopy);
+			}
+			
+			// connect cur with next
+			if ( cur.next != null ){
+				map.get(cur).next = map.get(cur.next);
+			}
+			
+			// connect cur with random
+			if ( cur.random != null ){
+				map.get(cur).random = map.get(cur.random);
+			}
+			
+			// next round
+			cur = cur.next;
 		}
 		
 		return map.get(head);
 	}
 }
+
+
+// public class Solution{
+// 	public RandomListNode copyRandomList(RandomListNode head){
+// 		if ( head == null )
+// 			return null;
+// 		
+// 		Map<RandomListNode, RandomListNode> map = new HashMap<>();
+// 		
+// 		RandomListNode cur = head;
+// 		while ( cur != null ){
+// 			map.put(cur, new RandomListNode(cur.label));
+// 			cur = cur.next;
+// 		}
+// 		
+// 		cur = head;
+// 		while ( cur != null ){
+// 			map.get(cur).next = map.get(cur.next);
+// 			map.get(cur).random = map.get(cur.random);
+// 			cur = cur.next;
+// 		}
+// 		
+// 		return map.get(head);
+// 	}
+// }
