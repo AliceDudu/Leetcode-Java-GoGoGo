db93ede60ba4e5c776f012d381422f12459c5619
Alice Yang <ligangmm@gmail.com>
2017-01-21 11:26:19 -0600

    Add files via upload

---
+++ Week05/62-Unique Paths.java
@@ -0,0 +1,30 @@
+public class Solution{
+	public int uniquePaths(int m, int n){
+		//corner
+		if ( m == 0 || n == 0 ){
+			return 0;
+		}
+		
+		//core
+		int[][] dp = new int[m][n];
+		
+		//initial
+		for ( int j = 0; j < n; j++ ){
+			dp[0][j] = 1;
+		}
+		
+		for ( int i = 0; i < m; i++ ){
+			dp[i][0] = 1;
+		}
+		
+		//rule
+		for ( int i = 1; i < m; i++ ){
+			for ( int j = 1; j < n; j++ ){
+				dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+			}
+		}
+		
+		return dp[m - 1][n - 1];
+		
+	}
+}
