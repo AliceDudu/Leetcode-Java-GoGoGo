2d8e5c06e8800f29137e783c9fc11aa6b2c92523
Alice Yang <ligangmm@gmail.com>
2017-01-29 22:28:09 -0600

    Add files via upload

---
+++ Week06/159-Longest Substring with At Most Two Distinct Characters.java
@@ -0,0 +1,62 @@
+public class Solution{	
+	public int length(String s){
+		//corner
+		if ( s == null || s.length() == 0 ){
+			return 0;
+		}
+	
+		int start = 0;
+		int end = 0;
+	
+		Map<Character, Integer> map = new HashMap<>();
+		int res = 0;
+	
+		while ( end < s.length() ){
+			if ( map.size() <= 2 ){
+				char c = s.charAt(end);
+				map.put(c, end);
+				end++;
+			}
+		
+			if ( map.size() > 2 ){
+				int left = s.length();
+				for ( int i : map.values() ){
+					left = Math.min(left, i);
+				}
+				map.remove(s.charAt(left));
+				start = left + 1;
+			}
+		
+			res = Math.max(res, end - start);
+		}
+	
+		return res;
+	
+	}
+
+}
+
+
+
+
+// 	public int length(String s){
+// 		int left = 0;
+// 		int right = -1;
+// 		int maxLen = 0;
+// 		
+// 		for ( int k = 1; k < s.length(); k++ ){
+// 			if ( s.charAt(k) == s.charAt(k - 1) ){
+// 				continue;
+// 			}
+// 			
+// 			if ( right > -1 && s.charAt(k) != s.charAt(right) ){
+// 				maxLen = Math.max(maxLen, k - left);
+// 				left = right + 1;
+// 			}
+// 			
+// 			right = k - 1;
+// 		}
+// 		
+// 		return Math.max(s.length() - left, maxLen);
+// 		
+// 	}
