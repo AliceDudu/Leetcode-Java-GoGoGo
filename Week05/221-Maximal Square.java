7b2a541e80d9d8b08dc15b410773b9362ecab41c
Alice Yang <ligangmm@gmail.com>
2017-02-13 11:47:03 -0600

    dp

---
+++ Week05/221-Maximal Square.java
@@ -0,0 +1,27 @@
+
+public class Solution{
+	public int maximalSquare( char[][] matrix ){
+		//corner
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+    		return 0;
+    	}
+    	
+		//core
+		int max = 0;
+		int n = matrix.length;
+		int m = matrix[0].length;
+		
+		int[][] dp = new int[n + 1][m + 1];
+		
+		for ( int i = 1; i <= n; i++ ){
+			for ( int j = 1; j <= m; j++ ){
+				if ( matrix[i - 1][j - 1] == '1' ){
+					dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+					max = Math.max(max, dp[i][j]);
+				}
+			}
+		}
+		
+		return max * max;
+	}
+}
