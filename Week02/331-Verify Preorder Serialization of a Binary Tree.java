977134873b7b1bf33df85d022a760d25845e60fa
Alice Yang <ligangmm@gmail.com>
2017-03-01 15:03:22 -0600

    stack

---
+++ Week02/331-Verify Preorder Serialization of a Binary Tree.java
@@ -0,0 +1,46 @@
+
+
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        // using a stack, scan left to right
+        // case 1: we see a number, just push it to the stack
+        // case 2: we see #, check if the top of stack is also #
+        // if so, pop #, pop the number in a while loop, until top of stack is not #
+        // if not, push it to stack
+        // in the end, check if stack size is 1, and stack top is #
+        if ( preorder == null ) {
+            return false;
+        }
+        
+        Stack<String> st = new Stack<>();
+        String[] strs = preorder.split(",");
+        
+        for ( int pos = 0; pos < strs.length; pos++ ) {
+        
+            String curr = strs[pos];
+            
+            while ( curr.equals("#") && !st.isEmpty() && st.peek().equals(curr) ) {
+//             	case 2.2: you see a #, while top of stack is #, you know you meet this # as right null child, 
+// 			you now cancel the sub tree (rooted as t, for example) with these two-# children. 
+// ---- if a #, you know you just cancelled a tree whose root, t, is the right child of u. 
+// 	So you continue to cancel sub tree of u, and the process goes on and on.
+                st.pop();
+                
+                if ( st.isEmpty() ) {
+                    return false;
+                }
+                
+                st.pop();
+            }
+            
+//             case 1: you see a number c, means you begin to expand a new tree rooted with c, you push it to stack
+// 			case 2.1: you see a #, while top of stack is a number, you know this # is a left null child, 
+// 			put it there as a mark for next coming node k to know it is being the right child.
+// case 2.2:  ---- if a number, say u, you know you just cancelled a node t which is left child of u. 
+// 	You need to leave a # mark to the top of stack. So that the next node know it is a right child.
+            st.push(curr);
+        }
+        
+        return st.size() == 1 && st.peek().equals("#");
+    }
+}
