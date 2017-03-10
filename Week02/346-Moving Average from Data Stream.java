eff88c001dee8b434b3aa43e1ebabf97df3dba92
Alice Yang <ligangmm@gmail.com>
2017-03-09 19:47:21 -0600

    queue

--- Week02/346-Moving Average from Data Stream.java
+++ Week02/346-Moving Average from Data Stream.java
@@ -1,3 +1,10 @@
+用一个q来模拟window
+遇到一个值，先加入到queue中，并加入到sum里
+queue的大小小于等于window的时候，直接求和，除以现有的个数
+q的大小超过window的时候，把head弹出，sum减去head的值，再做平均值
+返回平均值
+
+//week-02
 public class MovingAverage{
 	Deque<Integer> queue;
 	int sum;
@@ -13,16 +20,21 @@ public class MovingAverage{
 	}
 	
 	public double next(int val){
+// 		用一个q来模拟window
+// 		遇到一个值，先加入到queue中，并加入到sum里
 		queue.offer(val);
 		sum += val;
-			
+		
+// 		queue的大小小于等于window的时候，直接求和，除以现有的个数
 		if ( queue.size() <= windowSize ){
 			average = (double)sum / queue.size();
+// 		q的大小超过window的时候，把head弹出，sum减去head的值，再做平均值
 		}else{
 			int headval = queue.poll();
 			sum -= headval;
 			average = (double)sum / windowSize;
 		}
+// 		返回平均值
 		return average;
 	}	
 }
