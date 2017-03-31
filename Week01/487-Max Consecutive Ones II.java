4d6cfdaa6d162ca8869c02e0d59c1d8e387f3252
Alice Yang <ligangmm@gmail.com>
2017-03-31 16:04:16 -0500

    2 pointer

---
+++ Week01/487-Max Consecutive Ones II.java
@@ -0,0 +1,15 @@
+
+
+
+public int findMaxConsecutiveOnes(int[] nums) {
+     int maxConsecutive = 0, zeroLeft = 0, zeroRight = 0;
+     for (int i=0;i<nums.length;i++) {
+         zeroRight++;
+         if (nums[i] == 0) {
+             zeroLeft = zeroRight;
+             zeroRight = 0;
+         }
+         maxConsecutive = Math.max(maxConsecutive, zeroLeft+zeroRight); 
+     }
+     return maxConsecutive;
+}
